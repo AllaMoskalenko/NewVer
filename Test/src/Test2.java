6fd941fcd36d461ad2103fb9c1cf3b05ab2d432a
Alla Moskalenko <alla.a.moskalenko@gmail.com>
2018-03-18 16:41:16 +0200

    added text

--- Test/src/Test2.java
+++ Test/src/Test2.java
@@ -10,6 +10,7 @@ public class Test2 {
         String str = reader.readLine();
         char[] c = new char[str.length()];
         c = str.toCharArray();
+        System.out.println("Reversed string:");
         for (int i = str.length()-1; i>=0; i--) System.out.print(c[i]);
     }
 }
