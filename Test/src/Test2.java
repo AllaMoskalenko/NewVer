e5044121eb79e652e9ae9d3855afdf09793830ee
Alla Moskalenko <alla.a.moskalenko@gmail.com>
2018-03-18 16:33:02 +0200

    created new file

---
+++ Test/src/Test2.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//string in revers order
+public class Test2 {
+    public static void main (String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Write your string: ");
+    }
+}
